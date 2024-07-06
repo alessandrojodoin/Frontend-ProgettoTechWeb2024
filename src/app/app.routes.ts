91276d76b699b127a362bb6c53916dfaadbae2c1
Alessandro Jodoin <a,jodoin@studenti.unina.it>
2024-07-06 19:10:44 +0200

    UI updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
         path: "feed",
         title: "Hivemind",
         component: FeedComponent
+    },
+    {
+        path: "signup",
+        title: "Sign Up",
+        component: SignupComponent
+    },
+    {
+        path: "",
+        title: "Hivemind",
+        component: HomeComponent
+    },
+    {
+        path: "login",
+        title: "Login",
+        component: LoginComponent
     }
 ];
