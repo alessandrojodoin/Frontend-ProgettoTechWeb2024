eecb6dc22c3e90b0a7ce3711796f178e09298711
Alessandro Jodoin <a,jodoin@studenti.unina.it>
2024-07-07 02:42:11 +0200

    UI work

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  signupForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  })
 
+  onSubmit(){
+    console.log(this.signupForm.value.username);
+  }
 }
