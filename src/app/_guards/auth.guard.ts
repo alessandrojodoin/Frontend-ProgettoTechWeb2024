9fb01f42aa30e333e592f2e78a41b6d70b6e4f4b
Alessandro Jodoin <a,jodoin@studenti.unina.it>
2024-07-24 12:21:14 +0200

    Final changes and updated README

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -3,13 +3,16 @@ import { CanActivateFn, Router } from '@angular/router';
 
 import { UrlTree } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const toastr = inject(ToastrService);
   const auth = inject(AuthService);
   const router = inject(Router);
   if(auth.isUserAuthenticated()){
     return true;
   } else {
+    toastr.error("You must first login to access this page");
     return router.parseUrl("/login"); //return a UrlTree
   }
 };
